29996a82e38a6042fceba5f5f3774e238889e536
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2022-09-07 18:29:00 +0200

    Use the dedicated Dimensions component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { VOSviewerOnline } from 'vosviewer-online';
-import data from './JOI_2007-2016_co-authorship_network.json';
+import { VOSviewerOnlineDimensions } from 'vosviewer-online/dimensions';
+import data from './Dimensions_scientometrics_researcher_co-authorship_network.json';
 
 function App() {
   return (
     <div style={{
       position: 'absolute', top: '100px', left: '100px', width: '1100px', height: '700px', border: 'lightpink solid 5px'
     }}>
-      <VOSviewerOnline data={data} parameters={{ scale: 1.0 }} />
+      <VOSviewerOnlineDimensions data={data} parameters={{ scale: 1.0 }} />
     </div>
   );
 }
