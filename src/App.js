afb53d3a1e1c1533e6f475cb9bfd2f466dce0f12
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2022-09-13 17:19:12 +0200

    Use example network

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { VOSviewerOnlineDimensions } from 'vosviewer-online/dimensions';
-import data from './Dimensions_scientometrics_researcher_co-authorship_network.json';
+import data from './Dimensions_example_network.json';
 
 function App() {
   return (
     <div style={{
       position: 'absolute', top: '100px', left: '100px', width: '1100px', height: '700px', border: 'lightpink solid 5px'
     }}>
-      <VOSviewerOnlineDimensions data={data} parameters={{ scale: 1.0 }} />
+      <VOSviewerOnlineDimensions data={data} parameters={{ attraction: 2, repulsion: -1 }} />
     </div>
   );
 }
