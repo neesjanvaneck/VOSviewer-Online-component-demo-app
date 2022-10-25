14e7f60d1ff51aee255311e922220721bc2a640d
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2022-10-26 00:07:11 +0200

    Use the dedicated Zeta Alpha component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { VOSviewerOnline } from 'vosviewer-online';
-import data from './JOI_2007-2016_co-authorship_network.json';
+import { VOSviewerOnlineZetaAlpha } from 'vosviewer-online/zeta-alpha';
+import data from './Zeta-Alpha_ICLR2022.json';
 
 function App() {
   return (
     <div style={{
       position: 'absolute', top: '100px', left: '100px', width: '1100px', height: '700px', border: 'lightpink solid 5px'
     }}>
-      <VOSviewerOnline data={data} parameters={{ scale: 1.0 }} />
+      <VOSviewerOnlineZetaAlpha data={data} parameters={{ scale: 1.0 }} />
     </div>
   );
 }
