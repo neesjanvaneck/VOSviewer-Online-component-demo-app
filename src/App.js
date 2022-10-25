26596d8c14ed93cd164906fe0cf00c21475cecac
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2022-10-26 00:01:40 +0200

    Add Dimensions-Looker component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { VOSviewerOnlineDimensions } from 'vosviewer-online/dimensions';
 import data from './Dimensions_example_network.json';
+import data2 from './Dimensions_scientometrics_researcher_co-authorship_network.json';
 
 function App() {
   return (
-    <div style={{
-      position: 'absolute', top: '100px', left: '100px', width: '1100px', height: '700px', border: 'lightpink solid 5px'
-    }}>
-      <VOSviewerOnlineDimensions data={data} parameters={{ attraction: 2, repulsion: -1 }} />
-    </div>
+    <>
+      <div style={{
+        position: 'absolute', top: '10px', left: '10px', width: '800px', height: '700px', border: 'lightpink solid 5px'
+      }}>
+        <VOSviewerOnlineDimensions data={data} parameters={{ attraction: 2, repulsion: -1 }} />
+      </div>
+      <div style={{
+        position: 'absolute', top: '10px', left: '840px', width: '800px', height: '700px', border: 'lightpink solid 5px'
+      }}>
+        <VOSviewerOnlineDimensions data={data2} parameters={{ looker_ui: true }} />
+      </div>
+    </>
   );
 }
 
